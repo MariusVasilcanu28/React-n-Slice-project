29893584d0435ab47d308bf665fcfad00429780a
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2025-03-11 13:37:00 +0200

    Update Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -25,15 +25,13 @@ function Header() {
 
   return (
     <header
-      className={`flex flex-col items-center justify-between gap-3 border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:flex-row sm:px-6`}
+      className={`flex flex-col items-center justify-between gap-3 border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:flex-row sm:px-6 ${!username && "py-5"}`}
     >
       <Link to="/" className="tracking-widest">
         React 'n' Slice
       </Link>
-      <div
-        className={`flex justify-between gap-5 sm:gap-10 ${!username && "py-5"}`}
-      >
-        {username && (
+      {username && (
+        <div className={`flex justify-between gap-5 sm:gap-10`}>
           <>
             <SearchOrder />
             <div className="flex items-center gap-5">
@@ -44,8 +42,8 @@ function Header() {
               </Button>
             </div>
           </>
-        )}
-      </div>
+        </div>
+      )}
     </header>
   );
 }
